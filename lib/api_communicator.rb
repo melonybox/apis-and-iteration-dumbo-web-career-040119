093cb8a2e681430620a257eced3542e3dd1bf766
melonybox <grandmadcow@gmail.com>
2019-04-08 17:11:27 -0400

    Changes

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,10 +2,66 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+#At last page
+#response_hash["next"] == nil - no more pages
+#response_hash["count"] == 87 - total character entrys
+
 def get_character_movies_from_api(character_name)
   #make the web request
+
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+
+  counter = 1
+  films = []
+
+
+  while films.size <= 1 do
+
+    response_hash["results"].find do |item_name|
+
+      if item_name["name"].downcase == character_name.downcase
+        films = item_name["films"]
+        puts "Film found"
+        films.collect! do |film_url|
+          response_film_url = RestClient.get(film_url)
+          response_film_url_hash = JSON.parse(response_film_url)
+          response_film_url_hash["title"]
+        end
+        puts "Film Convert"
+        return films
+      elsif response_hash["next"] == nil
+        return puts "Character not found."
+      end
+
+    counter += 1
+
+    response_string = RestClient.get(response_hash["next"])
+    response_hash = JSON.parse(response_string)
+  end
+end
+
+
+  #films = []
+  #response_hash["results"].find do |item_name|
+  #
+  #  if item_name["name"].downcase == character_name.downcase
+  #    films = item_name["films"]
+  #  end
+  #end
+
+  #if response_hash["results"][0]["name"].downcase == character_name
+  #  films = response_hash["results"][0]["films"]
+  #end
+
+  #films.collect! do |film_url|
+  #  response_film_url = RestClient.get(film_url)
+  #  response_film_url_hash = JSON.parse(response_film_url)
+  #  response_film_url_hash["title"]
+  #end
+
+
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -18,8 +74,12 @@ def get_character_movies_from_api(character_name)
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
+
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |title|
+    puts title
+  end
 end
 
 def show_character_movies(character)
